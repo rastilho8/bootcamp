3c62a7bfe8e8481748193100e2aad0e3f4627bc2
unknown <h.david@campus.fct.unl.pt>
2020-11-11 00:15:07 +0000

    second workout

---
+++ work2/main.js
@@ -0,0 +1,37 @@
+
+const myForm = document.querySelector('.my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
+const msg = document.querySelector('#message');
+
+
+myForm.addEventListener('submit', onSubmit);
+
+function checkLength(el){
+
+}
+
+function onSubmit(e) {
+    e.preventDefault();
+
+
+    if (nameInput.value === '' || emailInput.value === '' || msg.value === '') {
+
+        console.error('error')
+
+
+    } else if (/[^\d\.]/.test(phoneInput.value)) {
+        console.error('Provide a valid number');
+
+    } else {
+
+        console.log(`Name: ${nameInput.value}, Email: ${emailInput.value}, Phone: ${phoneInput.value}, Message: ${msg.value}`);
+
+        nameInput.value = '';
+        emailInput.value = '';
+        phoneInput.value = ''
+        msg.value = ''
+    }
+}
+
