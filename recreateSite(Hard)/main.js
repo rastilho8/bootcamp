40cf32baa2ba65d2932c468a1a24925d03dd3832
unknown <h.david@campus.fct.unl.pt>
2020-11-19 17:20:11 +0000

    need to make posts

--- recreateSite(Hard)/main.js
+++ recreateSite(Hard)/main.js
@@ -1,16 +1,67 @@
 const changeContent = document.querySelector('.myContent');
+const contactContent = document.querySelector('#contactContent');
 const aboutContent = document.querySelector('#aboutContent');
+const homeContent = document.querySelector('#homeContent');
+const titleMain = document.querySelector('#titleMain');
+const subtitleMain = document.querySelector('#subtitleMain');
+const body = document.querySelector('body');
 
-aboutContent.addEventListener('click', doChange);
+contactContent.addEventListener('click', changeToContact);
 
-function loadContent(){
+function loadContent(path) {
   var xmlhttp = new XMLHttpRequest();
-  var pathToFile = 'contact.html';
-  xmlhttp.open("GET", pathToFile , false);
+  var pathToFile = `${path}`;
+  xmlhttp.open("GET", pathToFile, false);
   xmlhttp.send();
   return xmlhttp.responseText;
 }
 
-function doChange(e){
-  changeContent.innerHTML = loadContent();
+function changeToContact(e) {
+  //Fade out
+  body.style.opacity = 0;
+
+  // Wait for the transition 
+  setTimeout(function () {
+    titleMain.innerHTML = "Contact us!";
+    subtitleMain.innerHTML = "Have questions? We have answers."
+    changeContent.innerHTML = loadContent('contact.html');
+
+    // Fade in
+    body.style.opacity = 1;
+  }, 500);
 }
+
+aboutContent.addEventListener('click',changeToAbout);
+
+function changeToAbout(e){
+  //Fade out
+  body.style.opacity = 0;
+
+  // Wait for the transition 
+  setTimeout(function () {
+    titleMain.innerHTML = "About us!";
+    subtitleMain.innerHTML = "Want to know about us?"
+    changeContent.innerHTML = loadContent('aboutUs.html');
+
+    // Fade in
+    body.style.opacity = 1;
+  }, 500);
+}
+
+homeContent.addEventListener('click', changeToHome);
+
+function changeToHome(e){
+  //Fade out
+  body.style.opacity = 0;
+
+  // Wait for the transition 
+  setTimeout(function () {
+    titleMain.innerHTML = "Crypto Blog!";
+    subtitleMain.innerHTML = "Welcome to the crypto ship!?"
+   
+
+    // Fade in
+    body.style.opacity = 1;
+  }, 500);
+}
+
